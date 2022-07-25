7dc5e566f3cc445fa2de962f14370edbda1cc887
franciscocruz29 <franciscohuertas25@gmail.com>
2022-07-25 16:22:16 -0500

    Completed Sort by extension

---
+++ Electronic_station/sortByExtension.ts
@@ -0,0 +1,79 @@
+import assert from "assert";
+
+/* You are given a list of files. You need to sort this list by the file extension. The files with the same extension should be sorted by name.
+
+Some possible cases:
+
+  Filename cannot be an empty string;
+  Files without the extension should go before the files with one;
+  Filename ".config" has an empty extension and a name ".config";
+  Filename "config." has an empty extension and a name "config.";
+  Filename "table.imp.xls" has an extension "xls" and a name "table.imp";
+  Filename ".imp.xls" has an extension "xls" and a name ".imp".
+
+Input: A list of filenames.
+
+Output: A list of filenames. */
+
+function sortByExt(fileList: string[]) {
+  function getExt(filename: string): string {
+    const n = filename.lastIndexOf(".");
+    return n <= 0 ? "" : filename.slice(n);
+  }
+
+  function compare(filename1: string, filename2: string): number {
+    const ext1 = getExt(filename1);
+    const ext2 = getExt(filename2);
+    if (ext1 < ext2) return -1;
+    if (ext1 > ext2) return 1;
+    return filename1 < filename2 ? -1 : filename1 > filename2 ? 1 : 0;
+  }
+
+  return fileList.sort(compare);
+}
+
+console.log("Example:");
+console.log(sortByExt(["1.cad", "1.bat", "1.aa"]));
+
+assert.deepStrictEqual(sortByExt(["1.cad", "1.bat", "1.aa"]), [
+  "1.aa",
+  "1.bat",
+  "1.cad",
+]);
+assert.deepStrictEqual(sortByExt(["1.cad", "1.bat", "1.aa", "2.bat"]), [
+  "1.aa",
+  "1.bat",
+  "2.bat",
+  "1.cad",
+]);
+assert.deepStrictEqual(sortByExt(["1.cad", "1.bat", "1.aa", ".bat"]), [
+  ".bat",
+  "1.aa",
+  "1.bat",
+  "1.cad",
+]);
+assert.deepStrictEqual(sortByExt(["1.cad", "1.bat", ".aa", ".bat"]), [
+  ".aa",
+  ".bat",
+  "1.bat",
+  "1.cad",
+]);
+assert.deepStrictEqual(sortByExt(["1.cad", "1.", "1.aa"]), [
+  "1.",
+  "1.aa",
+  "1.cad",
+]);
+assert.deepStrictEqual(sortByExt(["1.cad", "1.bat", "1.aa", "1.aa.doc"]), [
+  "1.aa",
+  "1.bat",
+  "1.cad",
+  "1.aa.doc",
+]);
+assert.deepStrictEqual(sortByExt(["1.cad", "1.bat", "1.aa", ".aa.doc"]), [
+  "1.aa",
+  "1.bat",
+  "1.cad",
+  ".aa.doc",
+]);
+
+console.log("Coding complete!");
